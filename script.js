eb393023dcbca0756e9f42a2daafe4d545369140
YFSCIbrahimYildiz <yldzibrhim@gmail.com>
2025-05-06 14:46:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggle() {
+    let x = document.getElementById("navDiv");
+    let y = document.getElementById("headerDiv");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.height="auto"
+    } else {
+      x.style.display = "none";
+    }
+  }
